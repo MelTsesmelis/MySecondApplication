383c7f88b60ce8fe313bcabdecc875f1ead1217b
meletis <meletistsesmelis452001@gmail.com>
2022-02-08 01:17:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mysecondapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.mysecondapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.mybutton).setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/mysecondapplication/MapsActivity.java
@@ -0,0 +1,80 @@
+package com.example.mysecondapplication;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.filament.View;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.mysecondapplication.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    private static Context context;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ContentResolver contentResolver = getContentResolver();
+                        Uri uri = Uri.parse("com.example.geofencing/CORDINATES");
+                        Cursor cursor = contentResolver.query(
+                                uri,
+                                null,
+                                null,
+                                null,
+                                null);
+                        if (cursor.moveToFirst()) {
+                            while (cursor.moveToNext()) {
+                                Log.d("Cursor", cursor.getString(0));
+                            }
+                        } else {
+                            Log.d("ERROR", "nothing found in this cursor");
+                        }
+                    }
+                });
+                thread.start();
+            }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Eleusis and move the camera
+        LatLng eleusis = new LatLng(38.041275, 23.541812);
+        mMap.addMarker(new MarkerOptions().position(eleusis).title("Marker in Eleusis"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(eleusis));
+    }
+}
